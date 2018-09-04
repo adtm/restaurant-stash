3ecf88985612d455481d71aa9a0cfe3fa758e2d6
adtm <tomas.eglinskas@gmail.com>
2018-09-04 23:51:12 +0300

    feat(): Setup TSLint, Prettier

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -5,5 +5,5 @@ import { Hello } from "../src/components/Hello";
 
 ReactDOM.render(
   <Hello compiler="Typescript" framework="React" />,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
